1e1cd5e8e9b6b809d2bd7f2c84fbff2e34e6d026
Karthrand <735867550@qq.com>
2018-07-16 22:35:39 +0800

    完成设置页显示

--- app/src/main/java/com/oude/setting/MainActivity.java
+++ app/src/main/java/com/oude/setting/MainActivity.java
@@ -1,14 +1,123 @@
 package com.oude.setting;
 
-import android.app.*;
-import android.os.*;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.support.v7.app.ActionBar;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import java.util.*;
+import android.widget.*;
+import android.view.*;
+import android.preference.*;
 
-public class MainActivity extends Activity 
+public class MainActivity extends AppCompatPreferenceActivity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setupActionBar();
     }
+    
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            //在actionbar上显示返回按钮
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    //确保屏幕有足够大空间去显示
+    private static boolean isXLargeTablet(Context context) {
+        return (context.getResources().getConfiguration().screenLayout
+            & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
+    }
+    
+    @Override
+    public boolean onIsMultiPane() {
+        return isXLargeTablet(this);
+    }
+    
+    //监听修改
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+
+                preference.setSummary(
+                    index >= 0
+                    ? listPreference.getEntries()[index]
+                    : null);
+
+            }else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+    };
+    
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+        //监听数据变化
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                                                                 PreferenceManager
+                                                                 .getDefaultSharedPreferences(preference.getContext())
+                                                                 .getString(preference.getKey(), ""));
+    }
+    
+    
+    @Override
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.pref_headers, target);
+    }
+    
+    //加载Fragment
+    protected boolean isValidFragment(String fragmentName) {
+        return PreferenceFragment.class.getName().equals(fragmentName)
+            || GeneralPreferenceFragment.class.getName().equals(fragmentName);
+    }
+    
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class GeneralPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_general);
+            setHasOptionsMenu(true);
+            bindPreferenceSummaryToValue(findPreference("example_text"));
+            bindPreferenceSummaryToValue(findPreference("example_list"));
+               
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                startActivity(new Intent(getActivity(), MainActivity.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+        
+    }
+    
+    
 }
