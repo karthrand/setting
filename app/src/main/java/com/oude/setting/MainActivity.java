f6cdaad24ddcb9683888afc23f56adfa654d32f4
Karthrand <735867550@qq.com>
2018-07-17 11:42:32 +0800

    增加设置获取和显示

--- app/src/main/java/com/oude/setting/MainActivity.java
+++ app/src/main/java/com/oude/setting/MainActivity.java
@@ -22,6 +22,7 @@ import java.util.*;
 import android.widget.*;
 import android.view.*;
 import android.preference.*;
+import android.content.*;
 
 public class MainActivity extends AppCompatPreferenceActivity
 {
@@ -30,33 +31,40 @@ public class MainActivity extends AppCompatPreferenceActivity
     {
         super.onCreate(savedInstanceState);
         setupActionBar();
+
     }
-    
-    private void setupActionBar() {
+
+    private void setupActionBar()
+    {
         ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
+        if (actionBar != null)
+        {
             //在actionbar上显示返回按钮
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
     //确保屏幕有足够大空间去显示
-    private static boolean isXLargeTablet(Context context) {
+    private static boolean isXLargeTablet(Context context)
+    {
         return (context.getResources().getConfiguration().screenLayout
             & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
     }
-    
+
     @Override
-    public boolean onIsMultiPane() {
+    public boolean onIsMultiPane()
+    {
         return isXLargeTablet(this);
     }
-    
+
     //监听修改
     private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
         @Override
-        public boolean onPreferenceChange(Preference preference, Object value) {
+        public boolean onPreferenceChange(Preference preference, Object value)
+        {
             String stringValue = value.toString();
 
-            if (preference instanceof ListPreference) {
+            if (preference instanceof ListPreference)
+            {
                 ListPreference listPreference = (ListPreference) preference;
                 int index = listPreference.findIndexOfValue(stringValue);
 
@@ -65,14 +73,17 @@ public class MainActivity extends AppCompatPreferenceActivity
                     ? listPreference.getEntries()[index]
                     : null);
 
-            }else {
+            }
+            else
+            {
                 preference.setSummary(stringValue);
             }
             return true;
         }
     };
-    
-    private static void bindPreferenceSummaryToValue(Preference preference) {
+
+    private static void bindPreferenceSummaryToValue(Preference preference)
+    {
         //监听数据变化
         preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
 
@@ -81,43 +92,63 @@ public class MainActivity extends AppCompatPreferenceActivity
                                                                  .getDefaultSharedPreferences(preference.getContext())
                                                                  .getString(preference.getKey(), ""));
     }
-    
-    
+
+
     @Override
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public void onBuildHeaders(List<Header> target) {
+    public void onBuildHeaders(List<Header> target)
+    {
         loadHeadersFromResource(R.xml.pref_headers, target);
     }
-    
+
     //加载Fragment
-    protected boolean isValidFragment(String fragmentName) {
+    protected boolean isValidFragment(String fragmentName)
+    {
         return PreferenceFragment.class.getName().equals(fragmentName)
             || GeneralPreferenceFragment.class.getName().equals(fragmentName);
     }
-    
+
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class GeneralPreferenceFragment extends PreferenceFragment {
+    public static class GeneralPreferenceFragment extends PreferenceFragment
+    {
         @Override
-        public void onCreate(Bundle savedInstanceState) {
+        public void onCreate(Bundle savedInstanceState)
+        {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_general);
             setHasOptionsMenu(true);
             bindPreferenceSummaryToValue(findPreference("example_text"));
             bindPreferenceSummaryToValue(findPreference("example_list"));
-               
+
         }
 
         @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
+        public boolean onOptionsItemSelected(MenuItem item)
+        {
             int id = item.getItemId();
-            if (id == android.R.id.home) {
+            if (id == android.R.id.home)
+            {
                 startActivity(new Intent(getActivity(), MainActivity.class));
                 return true;
             }
             return super.onOptionsItemSelected(item);
         }
-        
+
+        @Override
+        public void onStop()
+        {
+            super.onStop();
+            //返回时获取参数并使用Toast显示
+            SharedPreferences sp= PreferenceManager.getDefaultSharedPreferences(getActivity());
+            Boolean value1 = sp.getBoolean("example_switch", true);
+            String value2= sp.getString("example_text", getString(R.string.pref_default_name));
+            String value3 = sp.getString("example_list", "3s");
+            Toast.makeText(getActivity(), "启动页开关是否开启：" + value1 + "\n" + "用户名：" + value2 + "\n" + "启动页启动时间：" + value3, Toast.LENGTH_SHORT).show();
+            
+        }
+
+
     }
-    
-    
+
+
 }
